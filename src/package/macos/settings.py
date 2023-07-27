cbbdcc19ac73718d63f55af47048077b59505092
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-07-27 16:17:24 -0600

    Added build/deploy GitHub Action

---
+++ src/package/macos/settings.py
@@ -0,0 +1,9 @@
+files = ['dist/HydroLoader.app']
+symlinks = {'Applications': '/Applications'}
+
+background = 'builtin-arrow'
+hide = ['.background.tiff']
+icon_locations = {
+    'HydroLoader.app': (0, 110),
+    'Applications': (420, 110)
+}
