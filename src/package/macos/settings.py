6412e2051a826004211bd02599b49d20981f80e5
Kenneth Lippold <kjlippold@outlook.com>
2023-07-31 13:27:46 -0600

    Update settings.py

--- src/package/macos/settings.py
+++ src/package/macos/settings.py
@@ -1,9 +1,9 @@
-files = ['dist/HydroLoader.app']
+files = ['dist/StreamingDataLoader.app']
 symlinks = {'Applications': '/Applications'}
 
 background = 'builtin-arrow'
 hide = ['.background.tiff']
 icon_locations = {
-    'HydroLoader.app': (0, 110),
+    'StreamingDataLoader.app': (0, 110),
     'Applications': (420, 110)
 }
